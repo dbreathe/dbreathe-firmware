88a380dcfccc1d4272ccb5b58cfc27b143c6a671
Steve Dalton <steve@secluded.io>
2016-07-19 18:55:11 +1000

    Added NeoPixel

--- dbreathe-keto/dbreathe-keto.ino
+++ dbreathe-keto/dbreathe-keto.ino
@@ -22,6 +22,7 @@ const int gasValuePin = A0;
 #include <math.h>
 #include "DHT.h"
 const int humidityPin = 13;
+const int neoPixelPin = 12;
 #define DHTTYPE   DHT22   // DHT 22  (AM2302), AM2321
 
 // Initialize DHT sensor.
@@ -30,8 +31,10 @@ const int humidityPin = 13;
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(humidityPin, DHTTYPE);
 
+// Init single neopixel
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, neoPixelPin, NEO_GRB + NEO_KHZ800);
 
-int currentMode=1;
+int currentMode = 1;
 
 // Read gas variables
 int tempRead1 = 0;
@@ -57,36 +60,36 @@ int updateCountDown = UPDATE_TICKS;
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
 
-    switch(type) {
-        case WStype_DISCONNECTED:
-            USE_SERIAL.printf("[%u] Disconnected!\n", num);
-            break;
-        case WStype_CONNECTED:
-            {
-                IPAddress ip = webSocket.remoteIP(num);
-                USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-        
+  switch (type) {
+    case WStype_DISCONNECTED:
+      USE_SERIAL.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
         // send message to client
         webSocket.sendTXT(num, "Connected");
-            }
-            break;
-        case WStype_TEXT:
-            USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+      }
+      break;
+    case WStype_TEXT:
+      USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
 
-            // send message to client
-            // webSocket.sendTXT(num, "message here");
+      // send message to client
+      // webSocket.sendTXT(num, "message here");
 
-            // send data to all connected clients
-            // webSocket.broadcastTXT("message here");
-            break;
-        case WStype_BIN:
-            USE_SERIAL.printf("[%u] get binary lenght: %u\n", num, lenght);
-            hexdump(payload, lenght);
+      // send data to all connected clients
+      // webSocket.broadcastTXT("message here");
+      break;
+    case WStype_BIN:
+      USE_SERIAL.printf("[%u] get binary lenght: %u\n", num, lenght);
+      hexdump(payload, lenght);
 
-            // send message to client
-            // webSocket.sendBIN(num, payload, lenght);
-            break;
-    }
+      // send message to client
+      // webSocket.sendBIN(num, payload, lenght);
+      break;
+  }
 
 }
 
@@ -94,18 +97,18 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
 // Temperature sensor function, with humidity as a percentage and temperature as degrees Celcius.
 int tempHumidityCompensation(int value, float currentHumidity, float currentTemperature)
 {
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(currentHumidity) || currentHumidity > 100 || currentHumidity < 1) {
-      currentHumidity = 98;
-    }
-    if (isnan(currentTemperature) || currentTemperature > 50 || currentTemperature < -20) {
-      currentTemperature = 30;      
-    }
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(currentHumidity) || currentHumidity > 100 || currentHumidity < 1) {
+    currentHumidity = 98;
+  }
+  if (isnan(currentTemperature) || currentTemperature > 50 || currentTemperature < -20) {
+    currentTemperature = 30;
+  }
 
-    //function derrived from regression analysis of the graph in the datasheet
-    scalingFactor = (((currentTemperature * -0.02573)+1.898)+((currentHumidity*-0.011)+0.3966));
-    double scaledValue = value * scalingFactor;
-    return (int)scaledValue; 
+  //function derrived from regression analysis of the graph in the datasheet
+  scalingFactor = (((currentTemperature * -0.02573) + 1.898) + ((currentHumidity * -0.011) + 0.3966));
+  double scaledValue = value * scalingFactor;
+  return (int)scaledValue;
 }
 
 
@@ -118,7 +121,7 @@ int runningAverage(int previous, int input)
 
 
 // Convert the 1-1023 value from analog read to a voltage.
-float toVoltage(int reading){
+float toVoltage(int reading) {
   //constant derived from 5/1023 = 0.0048875
   float voltageConversionConstant = 0.0048875;
   float voltageRead = reading * voltageConversionConstant;
@@ -127,25 +130,25 @@ float toVoltage(int reading){
 
 
 // Convert the 1-1023 voltage value from gas sensor analog read to a resistance, 9800 is the value of the other resistor in the voltage divide.
-float toResistance(int reading){
-  float resistance = ((5/toVoltage(reading) - 1) * 9800);
+float toResistance(int reading) {
+  float resistance = ((5 / toVoltage(reading) - 1) * 9800);
   return resistance;
 }
 
 
 // Calculate the gas concentration relative to the resistance
-float acetoneResistanceToPPMf(float resistance){
+float acetoneResistanceToPPMf(float resistance) {
   double tempResistance = (double)resistance;
-  double PPM; 
+  double PPM;
   // Original threshold used by Jens was 50,000 ohm, but he had a bug in the next line anyway, so it's probably not an important threshold.
   //if (tempResistance > 50000) {
-    // BUG: In original code by Jens, he accidentally declared a new variable "PPM" instead of using the variable "PPM" of this function,
-    // so the next statement below was being ignored!
-    //double PPM = 0;
+  // BUG: In original code by Jens, he accidentally declared a new variable "PPM" instead of using the variable "PPM" of this function,
+  // so the next statement below was being ignored!
+  //double PPM = 0;
   //}
   //else {
-    double logPPM = (log10(tempResistance/R0)*-2.6)+2.7;
-    PPM = pow(10, logPPM);
+  double logPPM = (log10(tempResistance / R0) * -2.6) + 2.7;
+  PPM = pow(10, logPPM);
   //}
   return (float)PPM;
 }
@@ -172,57 +175,54 @@ void readsensor() {
 
 // Special Arduino initialization callback function
 void setup() {
-  Serial.begin(9600);      // open the serial port at 9600 bps:    
+  Serial.begin(9600);      // open the serial port at 9600 bps:
 
-  // TODO: Neopixel output
-    
-  pinMode(gasValuePin,INPUT);
+  pixels.begin(); // This initializes the NeoPixel library.
+
+  pixels.setPixelColor(0, pixels.Color(150, 0, 0));
+  pixels.show();
+
+  pinMode(gasValuePin, INPUT);
 
   // Initialize the DHT humidity sensor
   dht.begin();
-
-/*  
-  //Warmup, check that sensor is stabile.
-  while (checkIfSensorIsStabile() == false){
-    checkIfSensorIsStabile();
-  }
-*/
 
   // Initialize the WebSockets server
   // Serial.setDebugOutput(true);
   USE_SERIAL.setDebugOutput(true);
   USE_SERIAL.println();
   USE_SERIAL.println();
-  USE_SERIAL.println();  
+  USE_SERIAL.println();
   for (uint8_t t = 4; t > 0; t--) {
-      USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
-      USE_SERIAL.flush();
-      delay(1000);
+    USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
   }
   WiFiMulti.addAP("breath", "breath##");
-  while(WiFiMulti.run() != WL_CONNECTED) {
-      delay(100);
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    delay(100);
   }
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
-    
+
   Serial.println("Waiting for sensors to stabilize");
 
   // Just wait 30 seconds
   delay(30000);
-  
 
   // Read three times from gas sensor with 5ms between each reading
   readsensor();
 
   // Initialize the measurement
-  measurement = (tempRead1 + tempRead2 + tempRead3)/3;
-  
+  measurement = (tempRead1 + tempRead2 + tempRead3) / 3;
+
   Serial.println("Warmup finished");
   delay(1000);
 
-  Serial.println("Blow into mouthpiece to start. Results are shown in mmol/l ...");
+  pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  pixels.show();
 
+  Serial.println("Blow into mouthpiece to start. Results are shown in mmol/l ...");
 }
 
 
@@ -240,33 +240,35 @@ void loop()
   // Only do a sensor update roughly once per second
   if (updateCountDown <= 0) {
     updateCountDown = UPDATE_TICKS;
-    
-    // TODO: Neopixel output
-      
+
+    pixels.setPixelColor(0, pixels.Color(150, 0, 150));
+    pixels.show();
+
     //read three times from gas sensor with 5ms between each reading
     readsensor();
     measurement = runningAverage(measurement, tempRead1);
     measurement = runningAverage(measurement, tempRead2);
     measurement = runningAverage(measurement, tempRead3);
-  
+
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
     float humidity = dht.readHumidity();
     // Read temperature as Celsius (the default)
     float temperature = dht.readTemperature();
-    
+
     int compensated = tempHumidityCompensation(measurement, humidity, temperature);
     float resistance = toResistance(compensated);
     float ppmf = acetoneResistanceToPPMf(resistance);
     float mmol = ppmToMmol(ppmf);
-  
+
     // Publish our results through Wifi!
     char str[512];
-    snprintf(str, sizeof(str), "%d,%d,%d,%d,%d", (int)(mmol*1000.0f), (int)(ppmf*1000.0f), (int)(measurement*1.0f), (int)(humidity*1000.0f), (int)(temperature*1000.0f));
+    snprintf(str, sizeof(str), "%d,%d,%d,%d,%d", (int)(mmol * 1000.0f), (int)(ppmf * 1000.0f), (int)(measurement * 1.0f), (int)(humidity * 1000.0f), (int)(temperature * 1000.0f));
     webSocket.broadcastTXT(str);
 
-    // TODO: Neopixel output
-  
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
+
     // Check if any reads failed and exit early (to try again).
     if (isnan(humidity) || isnan(temperature)) {
       Serial.println("Failed to read from DHT sensor!");
@@ -280,7 +282,7 @@ void loop()
       Serial.print(" *C ");
     }
     Serial.print("      ");
-  
+
     Serial.print("Gas (V): ");
     Serial.print(measurement);
     Serial.print("   ");
@@ -291,11 +293,9 @@ void loop()
     Serial.print("   ");
     Serial.print("MMOL: ");
     Serial.print(mmol);
-  
-    Serial.println();  
+
+    Serial.println();
   }
-  
-    
 }
 
 
